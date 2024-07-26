87c3c6a3a741993c89fa1eb629b012e85aed0f7c
Simion <simion@simisoft.co.uk>
2024-07-26 20:40:55 +0100

    mappings to positions, serial numbers and serial ports

--- device_helpers/usb_to_json.sh
+++ device_helpers/usb_to_json.sh
@@ -13,41 +13,43 @@ get_ttyacm_info() {
 # Store ttyACM info in an awk array
 TTYACM_INFO=$(get_ttyacm_info)
 
-lsusb -v 2>/dev/null | awk -v ttyacm_info="$TTYACM_INFO" '
+# Read device mappings
+DEVICE_MAPPINGS=$(cat device_mappings.json)
+
+lsusb -v 2>/dev/null | awk -v ttyacm_info="$TTYACM_INFO" -v device_mappings="$DEVICE_MAPPINGS" '
 BEGIN { 
-    print "["
-    first = 1
     split("", props)
     split(ttyacm_info, ttyacm_array, "\n")
     for (i in ttyacm_array) {
         split(ttyacm_array[i], temp, ":")
         ttyacm_map[temp[2]] = temp[1]
     }
+
+    n = split(device_mappings, dm_array, /\n/)
+    in_array = 0
+    for (i = 1; i <= n; i++) {
+        if (dm_array[i] ~ /"device_id"/) {
+            match(dm_array[i], /"device_id": *"([^"]+)"/, id)
+            device_id = id[1]
+        }
+        if (dm_array[i] ~ /"device_serial_no"/) {
+            match(dm_array[i], /"device_serial_no": *"([^"]+)"/, serial)
+            device_serial = serial[1]
+            device_map[device_serial] = device_id
+        }
+    }
 }
 function print_props() {
-    first_prop = 1
-    for (p in props) {
-        if (!first_prop) printf ","
-        first_prop = 0
-        printf "\"%s\": \"%s\"", p, props[p]
-    }
-    if (props["iSerial"] in ttyacm_map) {
-        if (!first_prop) printf ","
-        printf "\"serialPort\": \"%s\"", ttyacm_map[props["iSerial"]]
+    if (props["iSerial"] in device_map && props["iSerial"] in ttyacm_map) {
+        printf "{\"device_serial_no\": \"%s\", \"device_id\": \"%s\", \"serial_port\": \"%s\"}\n", 
+               props["iSerial"], device_map[props["iSerial"]], ttyacm_map[props["iSerial"]]
     }
     split("", props)
 }
 /^Bus/ { 
-    if (!first) {
+    if (length(props) > 0) {
         print_props()
-        print "}"
     }
-    first = 0
-    print (NR > 2 ? "," : "") "{"
-    printf "\"bus\": \"%s\",", $2
-    dev = $4
-    sub(":$", "", dev)  # Remove trailing colon from device number
-    printf "\"device\": \"%s\",", dev
 }
 /idVendor/ { props["idVendor"] = $2 }
 /idProduct/ { props["idProduct"] = $2 }
@@ -59,13 +61,11 @@ function print_props() {
     product = substr($0, index($0,$3))
     gsub(/^[ \t]+|[ \t]+$/, "", product)
     gsub(/"/, "\\\"", product)
-    if (product != "") props["iProduct"] = product
+    if (product != "" && product ~ /STM32/) props["iProduct"] = product
 }
 END { 
-    if (!first) {
+    if (length(props) > 0) {
         print_props()
-        print "}"
     }
-    print "]" 
 }
-' | jq '[.[] | select(.iProduct | contains("STM32"))]'
+' | jq -s '.'
