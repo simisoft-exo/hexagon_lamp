a8369c0af72c7c504ca153f5b20a457207919b3d
Simion <simion@simisoft.co.uk>
2024-07-26 18:20:23 +0100

    can link serial to serial port

--- device_helpers/usb_to_json.sh
+++ device_helpers/usb_to_json.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-lsusb -v 2>/dev/null | awk '
+# Function to get ttyACM information
+get_ttyacm_info() {
+    for ttyacm in /dev/ttyACM*; do
+        if [ -e "$ttyacm" ]; then
+            serial=$(udevadm info -n $ttyacm | grep ID_SERIAL_SHORT | cut -d= -f2)
+            echo "$ttyacm:$serial"
+        fi
+    done
+}
+
+# Store ttyACM info in an awk array
+TTYACM_INFO=$(get_ttyacm_info)
+
+lsusb -v 2>/dev/null | awk -v ttyacm_info="$TTYACM_INFO" '
 BEGIN { 
-    print "[" 
+    print "["
     first = 1
     split("", props)
+    split(ttyacm_info, ttyacm_array, "\n")
+    for (i in ttyacm_array) {
+        split(ttyacm_array[i], temp, ":")
+        ttyacm_map[temp[2]] = temp[1]
+    }
 }
 function print_props() {
     first_prop = 1
@@ -12,6 +30,10 @@ function print_props() {
         if (!first_prop) printf ","
         first_prop = 0
         printf "\"%s\": \"%s\"", p, props[p]
+    }
+    if (props["iSerial"] in ttyacm_map) {
+        if (!first_prop) printf ","
+        printf "\"serialPort\": \"%s\"", ttyacm_map[props["iSerial"]]
     }
     split("", props)
 }
@@ -23,7 +45,9 @@ function print_props() {
     first = 0
     print (NR > 2 ? "," : "") "{"
     printf "\"bus\": \"%s\",", $2
-    printf "\"device\": \"%s\",", $4
+    dev = $4
+    sub(":$", "", dev)  # Remove trailing colon from device number
+    printf "\"device\": \"%s\",", dev
 }
 /idVendor/ { props["idVendor"] = $2 }
 /idProduct/ { props["idProduct"] = $2 }
@@ -44,4 +68,4 @@ END {
     }
     print "]" 
 }
-'
+' | jq '[.[] | select(.iProduct | contains("STM32"))]'
